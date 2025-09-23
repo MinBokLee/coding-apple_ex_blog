be333f449176789169b38138742a97da8568b528
MinbokLee <lik707@naver.com>
2025-09-23 13:00:22 +0900

    step1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  let [글제목,b] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 톡학']);
+  let post = '강남 우동 맛집';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nev">
+        <h4>React Blog</h4>
+      </div>
+      <div className="list">
+        <h4> { 글제목 } </h4>
+      <p>2월 17일 발행</p>
+      </div>
+      <div className="list">  
+        <h4> { 글제목 } </h4>
+      <p>2월 17일 발행</p>
+      </div>
+        <div className="list">
+        <h4> { 글제목 } </h4>
+      <p>2월 17일 발행</p>
+      </div>
     </div>
+    
   );
 }
 
