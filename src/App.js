08b62061fc8165d534d1fcfe2b12d553bb2e097f
MinbokLee <lik707@naver.com>
2025-09-29 11:36:25 +0900

    modal component

--- src/App.js
+++ src/App.js
@@ -14,17 +14,24 @@ function 함수(){
 }
 
 
+function 제목정렬하기(){
+  console.log("제목정렬하기");
+  var newArray =[...글제목]; 
+  newArray[0] = '강남 우동 맛집'
+  newArray[1] = '여자코트 추천'
+  newArray[2] = '파이썬 독학'
+  글제목변경(newArray);
+}
+
   return (
     <div className="App">
       <div className="black-nev">
-        <h4>React Blog</h4>
+        <h4>개발 블로그</h4>
       </div>
 
-      <button onClick={() =>{
-        let copy = [...글제목];
-        copy[0] = '여자코트 추천';
-      글제목변경(copy);
-      }}>글수정</button>
+
+
+      <button onClick={ 제목정렬하기 }> 버튼</button>
 
       <div className="list">
         <h4> { 글제목[0] } <span onClick={함수}> 👍 </span> {따봉} </h4>
@@ -40,9 +47,22 @@ function 함수(){
         <h4> { 글제목[2] } </h4>
       <p>2월 17일 발행</p>
       </div>
+    
+    <Modal></Modal>
+    
     </div>
     
   );
+}
+
+function Modal(){
+  return (
+        <div className="modal">
+      <h2>날짜</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
